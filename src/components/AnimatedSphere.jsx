0e59d34a21152ce3f929a3ffd903e4c4648f7aaf
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-18 11:47:15 +1100

    add animated blob

--- src/components/AnimatedSphere.jsx
+++ src/components/AnimatedSphere.jsx
@@ -1,7 +1,17 @@
 import { Sphere, MeshDistortMaterial } from "@react-three/drei";
 
 const AnimatedSphere = () => {
-  return <Sphere visible args={[1, 100, 200]} scale={2} />;
+  return (
+    <Sphere visible args={[1, 100, 200]} scale={2}>
+      <MeshDistortMaterial
+        color="#8352FD"
+        attach="material"
+        distort={0.3}
+        speed={3}
+        roughness={0.2}
+      />
+    </Sphere>
+  );
 };
 
 export default AnimatedSphere;
