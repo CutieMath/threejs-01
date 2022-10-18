01a482c74ab8432d3a0741483ce02495fb46eeac
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-18 11:12:33 +1100

    resize the box and add rotation

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import styled from "styled-components";
 
 import Background from "./components/Background";
 import TextSection from "./components/TextSection";
+import Box from "./components/Box";
+import { Canvas } from "@react-three/fiber";
 
 export default function App() {
   return (
     <Wrapper className="App">
       <Background />
       <TextSection />
+      <Canvas className="canvas">
+        <Box />
+      </Canvas>
     </Wrapper>
   );
 }
@@ -16,4 +21,8 @@ export default function App() {
 const Wrapper = styled.div`
   position: relative;
   background: #1f1144;
+
+  canvas {
+    height: 500px;
+  }
 `;
